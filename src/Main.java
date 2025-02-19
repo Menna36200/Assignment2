f5074e57425bb341f202ff7ee081cc6483400186
Menna36200 <menna.mamdouh36200@gmail.com>
2025-02-19 16:03:29 +0200

    1

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        System.out.println("EnterThe Number: ");
+        int num;
+        boolean n = false;
+        Scanner input = new Scanner(System.in);
+
+        num = input.nextInt();
+        for(int i =2 ;i<num;i++)
+        {
+            if(num % i ==0 )
+            {
+           n = true;
+           break;
+            }
+        }
+        if(n==true)
+        {
+            System.out.println("The " +num+ " is not prime number");
+        }
+        else
+        {
+            System.out.println("The " +num+ " is prime number");
+        }
+    }
+
+}
