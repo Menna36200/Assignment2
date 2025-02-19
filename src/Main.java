ca2a520758c2503c7a403fa60879dd8b6e82e57f
Menna36200 <menna.mamdouh36200@gmail.com>
2025-02-19 16:05:09 +0200

    1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("EnterThe Number: ");
+        System.out.println("Enter The Number: ");
         int num;
         boolean n = false;
         Scanner input = new Scanner(System.in);
